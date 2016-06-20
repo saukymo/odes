88752cb699a6512a31ce6dbfc38498250004efc2
saukymo <saukymo@163.com>
2016-06-20 12:07:30 +0800

    almost finish

--- app.py
+++ app.py
@@ -3,6 +3,9 @@ import psycopg2 as pg
 from flask import Flask, jsonify
 import ConfigParser
 
+
+MAXID = 305
+
 cf = ConfigParser.ConfigParser()
 cf.read('dev.conf')
 
@@ -21,11 +24,20 @@ def index():
 
 @app.route("/odes-api/<int:id>")
 def get_ode_by_id(id):
+	min_id, max_id = id - 1, id + 1
+	if (id == 1):
+		min_id = MAXID
+	if (id == MAXID):
+		max_id = 1
 	script = """
-		WITH s AS (SELECT * FROM odes where id = %s) SELECT row_to_json(s) FROM s;
+		WITH s AS (SELECT * FROM odes where id in (%s, %s, %s)) SELECT array_to_json(array_agg(row_to_json(s))) FROM s;
 	"""
-	cu.execute(script, (id,))
-	res = cu.fetchall()
+	cu.execute(script, (min_id, id, max_id))
+	res = cu.fetchone()
+	if (id == 1):
+		res[0][0], res[0][1], res[0][2] = res[0][2], res[0][0], res[0][1]
+	if (id == MAXID):
+		res[0][0], res[0][1], res[0][2] = res[0][1], res[0][2], res[0][0]
 	if res is None:
 		raise ValueError
 	else:
--- js/index.js
+++ js/index.js
@@ -18,11 +18,23 @@ var getUrlParameter = function getUrlParameter(sParam) {
     }
 };
 
+function getRandomInt(x) {
+  return Math.floor(Math.random() * x) + 1;
+}
+
 url="/odes-api/ode_id".replaceAll("ode_id", getUrlParameter('id'));
 
 $.get(url, function(data){
 	console.log(data);
-	$('#title').text(data[0].title);
-	$('#fulltext').text(data[0].full_text);
-	$('#category').append("<li><a href=" + "#" + ">" + data[0].p_class + "</a></li>")
+	$('#random_ode').attr("href", "/odes/index.html?id=ode_id".replaceAll("ode_id", getRandomInt(305)));
+	$('#title').text(data[1].title);
+	$('#fulltext').text(data[1].full_text);
+	$('#category').append("<li><a href=\"" + "#" + "\" class=\"btn-lg\">" + data[1].p_class + "</a></li>")
+	$('#category').append("<li><a href=\"" + "#" + "\" class=\"btn-lg\">" + data[1].p_group + "</a></li>")
+	if (data[1].p_subgroup)
+	{
+		$('#category').append("<li><a href=\"" + "#" + "\" class=\"btn-lg\">" + data[1].p_subgroup + "</a></li>")
+	}
+	$('#left_ode').text("上一篇：" + data[0].title).attr("href", "/odes/index.html?id=ode_id".replaceAll("ode_id", data[0].id));
+	$('#right_ode').text("下一篇：" + data[2].title).attr("href", "/odes/index.html?id=ode_id".replaceAll("ode_id", data[2].id));
 })
